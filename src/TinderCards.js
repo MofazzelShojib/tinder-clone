062d78fb92e6f5f82c1217a0b8db3f4773b4b83f
Mofazzel Hossain Shojib <shojib178@gmail.com>
2021-12-29 18:46:27 +0600

    tinder mern front-end
    Tinder MERN stack front-end, hosted in Firebase

---
+++ src/TinderCards.js
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react';
+import TinderCard from 'react-tinder-card';
+import "./TinderCards.css";
+import axios from "./axios";
+
+function TinderCards() {
+
+    const [people, setPeople] = useState([]);
+        /*{
+           name:"Jennifer Lawrence",
+           url:"https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/09/27/18/jennifer-lawrence.jpg?width=1200",
+          }*/
+    useEffect(() => {
+        async function fetchData() {
+            const req = await axios.get("/tinder/cards");
+            setPeople(req.data);
+        }
+        fetchData();        
+    }, []);
+
+    const swiped = (direction, nameToDelete) => {
+        console.log("removing: " + nameToDelete);
+        //setLastDirection(direction);
+    };
+
+    const outOfFrame = (name) => {
+        console.log(name + "left the screen!");
+    };
+
+    return (
+        <div className="tinderCards">
+            <div className="tinderCards__container">
+                {people.map((person) => (
+                    <TinderCard
+                        className="swipe"
+                        key={person.name}
+                        preventSwipe={["up","down"]}
+                        onSwipe={(dir) => swiped(dir, person.name)}
+                        onCardLeftScreen={() => outOfFrame(person.name)}
+                        >
+                        <div style={{backgroundImage: `url(${person.imgUrl})`}}
+                             className="card"
+                            >
+                            <h3>{person.name}</h3>
+                        </div>
+
+                    </TinderCard>
+                ))}
+            </div> 
+            
+        </div>
+    );
+}
+
+export default TinderCards;
